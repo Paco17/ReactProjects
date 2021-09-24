1ce0dc5c7936223a618df760ff2de1021c7c72e2
Paco17 <A01636425@itesm.mx>
2021-09-23 22:16:42 -0500

    Formulario done

--- music/src/App.js
+++ music/src/App.js
@@ -1,9 +1,10 @@
 import React, {Fragment} from 'react';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
     <Fragment>
-      
+      <Formulario/>
     </Fragment>
   );
 }
