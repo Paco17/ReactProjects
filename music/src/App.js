6fa76929a3372790862eedb2b9ddf9eb40f2289e
Paco17 <A01636425@itesm.mx>
2021-09-23 23:45:17 -0500

    Music Project done

--- music/src/App.js
+++ music/src/App.js
@@ -1,6 +1,7 @@
 import React, {Fragment, useState,useEffect} from 'react';
 import Formulario from './components/Formulario';
 import Cancion from './components/Cancion';
+import Info from './components/Info';
 import axios from 'axios';
 
 
@@ -9,6 +10,7 @@ function App() {
   //definir el state
   const[busquedaletra, guardarBusquedaLetra] = useState ({});
   const[letra, guardarLetra] = useState('');
+  const[info, guardarInfo] =useState({});
 
   useEffect(() => {
     if(Object.keys(busquedaletra).length===0) return;
@@ -17,16 +19,24 @@ function App() {
       const {artista, cancion} =busquedaletra
 
       const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+      const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
 
-      const resultado = await axios(url);
 
-      console.log(resultado.data.lyrics);
-      guardarLetra(resultado.data.lyrics);
+      //Utilizacion de Promise
+      const[letra, informacion] = await Promise.all([
+        axios(url),
+        axios(url2)
+
+      ]);
+
+      
+      guardarLetra(letra.data.lyrics);
+      guardarInfo(informacion.data.artists[0]);
     } 
 
     consultarApiLetra();
 
-  }, [busquedaletra])
+  }, [busquedaletra, info])
 
   return (
     <Fragment>
@@ -35,9 +45,11 @@ function App() {
       />
 
       <div className="container">
-        <div className="row">
+        <div className="row pt-4">
           <div className="col-md-6">
-
+            <Info
+              info = {info}
+            />
           </div>
           <div className="col-md-6">
             <Cancion 
